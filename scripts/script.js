5604a9fb1a2639dcaef2f9e94988172f9856018b
EC2 Default User <ec2-user@ip-172-31-10-147.ap-northeast-1.compute.internal>
2021-04-13 15:29:51 +0000

    submit2

--- scripts/script.js
+++ scripts/script.js
@@ -6,29 +6,17 @@
 });
     
 
+// geolocation
+navigator.geolocation.getCurrentPosition(success, fail);
 
 function success(pos) {
+    ajaxRequest(pos.coords.latitude, pos.coords.longitude);
     const lat = pos.coords.latitude;
     const lng = pos.coords.longitude;
     const accuracy = pos.coords.accuracy;
 
     $('#loc').text(`緯度：${lat} 経度：${lng}`);
     $('#accuracy').text(accuracy);
-}
-
-function fail(error) {
-    alert('位置情報の取得に失敗しました。エラーコード：' + error.code);
-}
-
-navigator.geolocation.getCurrentPosition(success, fail);
-
-
-
-// geolocation
-navigator.geolocation.getCurrentPosition(success, fail);
-
-function success(pos) {
-    ajaxRequest(pos.coords.latitude, pos.coords.longitude);
 }
 
 function fail(error) {
